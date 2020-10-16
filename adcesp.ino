eb80f1b744b57ee0aa9eb97330088a4f818db537
majuzabbal <58271501+majuzabbal@users.noreply.github.com>
2020-10-16 20:24:40 -0300

    Add files via upload

---
+++ adcesp.ino
@@ -0,0 +1,21 @@
+const byte interruptPin = 18;
+int analog_value = 0;
+#define ANALOG_PIN_0 36
+ 
+void setup(){
+  Serial.begin(115200);
+  pinMode(2, OUTPUT);
+  pinMode(interruptPin, INPUT);
+  
+  attachInterrupt(digitalPinToInterrupt(interruptPin), interruptFunction, RISING);
+}
+ 
+void loop(){
+  delay(500);
+  Serial.println(map(analog_value, 0, 4095, 0, 1023));
+}
+
+void interruptFunction(){
+//delayMicroseconds(3);
+analog_value = analogRead(ANALOG_PIN_0);
+}
