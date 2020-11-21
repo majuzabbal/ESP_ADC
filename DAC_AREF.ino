0785642f4c6c8fee11236cb74d98c59550c9c142
majuzabbal <58271501+majuzabbal@users.noreply.github.com>
2020-11-21 07:33:25 -0300

    Update DAC_AREF.ino

--- DAC_AREF.ino
+++ DAC_AREF.ino
@@ -1,6 +1,9 @@
 #include <Wire.h>
 #include <Adafruit_MCP4725.h>
-int voltage[] = {0, 683, 1365, 2047, 2730, 3413, 4095};
+int voltage[] = {0, 683, 1365, 2047, 3413, 4095};
+int interruptPin = 34;  
+volatile bool flag = false;
+int counter = 0;
 
 Adafruit_MCP4725 dac;
 
@@ -9,15 +12,20 @@ void setup(void) {
   Serial.println("Hello!");
 
   dac.begin(0x62);
-    
+
+  attachInterrupt(digitalPinToInterrupt(interruptPin), interruptFunction, RISING);
 }
 
 void loop(void) {
-    int counter;
-
-    for (counter = 0; counter < 7; counter++)
-    {
-      dac.setVoltage(voltage[counter], false);
-      delay(500);
+    while(counter < 7){
+      if(flag){
+        dac.setVoltage(voltage[counter], false);
+        counter++;
+        flag = false;
+      }
     }
+}
+
+void interruptFunction() {
+  flag = true;
 }
